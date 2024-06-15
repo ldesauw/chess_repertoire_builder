55ab3c526b6c6fc04cc2f279f85307ea8b987e7d
Aeddis Desauw <aeddis@deuxfleurs.fr>
2024-06-15 10:46:34 +0200

    Add comment Move.h

--- include/object/move.h
+++ include/object/move.h
@@ -1,5 +1,6 @@
 #ifndef MOVE_H_
 #define MOVE_H_
+#include <string>
 
 #define Move std::string
 #define MovePart std::string
@@ -7,22 +8,31 @@
 
 class MoveEntity {
 private:
-    const Move      _move;
-    const MovePart  _piece;
-    const MovePart  _source_file;
-    const MovePart  _source_rank;
-    const MovePart  _destination_file;
-    const MovePart  _destination_rank;
-    const MovePart  _promotion_piece;
-    const MoveSpec  _isEating;
-    const MoveSpec  _isCheck;
-    const MoveSpec  _isMate;
-    const MoveSpec  _isCastle;
-    const MoveSpec  _isLongCastle;
-    const MoveSpec  _isPromoting;
+  // The move in PGN format
+  const Move _move;
+
+  // Move descriptor interpreted from the description
+  const MovePart _piece;
+  const MovePart _source_file;
+  const MovePart _source_rank;
+  const MovePart _destination_file;
+  const MovePart _destination_rank;
+  const MovePart _promotion_piece;
+  const MoveSpec _isEating;
+  const MoveSpec _isCheck;
+  const MoveSpec _isMate;
+  const MoveSpec _isCastle;
+  const MoveSpec _isLongCastle;
+  const MoveSpec _isPromoting;
 
 public:
-    MoveEntity(const Move &move);
+  /**
+   * Instantiate a MoveEntity from a move in PGN format
+   *
+   * @param move The description of the move in PGN format
+   * @throws invalid_argument If the move is not in correct PGN format
+   */
+  MoveEntity(const Move &move);
 }
 
 #endif // MOVE_H_
